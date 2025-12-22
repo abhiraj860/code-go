28a17f1db5fd15e7ff94e36a5b3f09847faf3869
abhiraj <abhiaditya860@gmail.com>
2025-12-22 23:25:33 +0530

    ll

--- main.go
+++ main.go
@@ -2,17 +2,39 @@ package main
 
 import "fmt"
 
-type Person struct {
-	name string
-	age int
+type Node struct {
+	val int
+	next *Node
 }
 
-func initPerson() *Person {
-	p := Person{name:"Abhiraj", age: 56}
-	fmt.Printf("%p\n", &p)
-	return &p
+type LinkedList struct {
+	head *Node
+	length int
+}
+
+func (T * LinkedList) prepend(node *Node) {
+	n := T.head
+	T.head = node
+	node.next = n
+}
+
+func (T * LinkedList) readList() {
+	p := T.head
+	for p != nil {
+		fmt.Println(p.val)
+		p = p.next
+	}
 }
 
 func main() {
-	fmt.Printf("%p\n", initPerson())
+	list := LinkedList{}
+	node1 := Node{val:4}
+	node2 := Node{val:5}
+	node3 := Node{val:100}
+	node4 := Node{50,nil}
+	list.prepend(&node1)
+	list.prepend(&node2)
+	list.prepend(&node3)
+	list.prepend(&node4)
+	list.readList()
 }
