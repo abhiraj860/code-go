18f7ec61fe5e5e4e04cde91dfb0b86890d9198c8
abhiraj <abhiaditya860@gmail.com>
2025-12-22 17:37:13 +0530

    ptrs

--- main.go
+++ main.go
@@ -2,52 +2,17 @@ package main
 
 import "fmt"
 
-func SlicesIndex[S ~[]E, E comparable](s S, v E) int {
-	for i := range s {
-		if v == s[i] {
-			return i
-		}
-	}
-	return -1
-}
-
-type element[T any] struct {
-	next *element[T]
-	val  T
-}
-
-type List[T any] struct {
-	head, tail *element[T]
-}
-
-func (lst *List[T]) Push(v T) {
-	if lst.tail == nil {
-		lst.head = &element[T]{val: v}
-		lst.tail = lst.head
-	} else {
-		lst.tail.next = &element[T]{val: v}
-		lst.tail = lst.tail.next
-	}
-}
-
-func (lst *List[T]) AllElements() []T {
-	var elems []T
-	for e := lst.head; e != nil ; e = e.next {
-		elems = append(elems, e.val)
-	}
-	return elems
-}
-
 func main() {
-	var s = []string{"foo", "bar", "zoo"}
+ i, j := 42, 2701
+ fmt.Println(&i, &j)
+ 
+ p := &i
 
-	fmt.Println("index of zoo: ", SlicesIndex(s, "zoo"))
-	_ = SlicesIndex[[]string, string](s, "zoo")
+ fmt.Println(*p)
+ *p = 21
+ fmt.Println(i)
 
-	lst := List[int]{}
-	lst.Push(10)
-	lst.Push(13)
-	lst.Push(23)
-	fmt.Println("list:", lst.AllElements())
-
+ p = &j
+ *p = *p / 37
+ fmt.Println(j)
 }
