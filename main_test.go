8bbae23f57ff51b878926b01a15e0f74a7106abd
abhiraj <abhiaditya860@gmail.com>
2025-12-25 01:08:23 +0530

    goShed

---
+++ main_test.go
@@ -0,0 +1,12 @@
+package main
+
+import "testing"
+
+func TestPackItems(t * testing.T) {
+	totalItems := PackItems(2000)
+	expectedTotal := 2000
+	if totalItems != expectedTotal {
+		t.Errorf("Expected total: %d, Actual value %d", expectedTotal, totalItems)
+	}
+}
+
