029a10a3eac4c4e04e170a53963dcc4b13fde5be
abhiraj <abhiaditya860@gmail.com>
2025-12-24 22:48:48 +0530

    gorouting

--- main.go
+++ main.go
@@ -2,17 +2,24 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"sync"
 )
 
-func say(s string ) {
-	for i := 1; i <= 5; i++ {
-		time.Sleep(500 * time.Millisecond)
+func say(s string, wg * sync.WaitGroup) {
+	defer wg.Done()
+	for i := 0; i < 5; i++ {
 		fmt.Println(s)
 	}
 }
 
 func main() {
-	say("Hell0")
-	go say("golang")
+
+	wg := sync.WaitGroup{}
+	wg.Add(2)
+
+	go say("Hello", &wg)
+	go say("World", &wg)
+
+	wg.Wait()
+	fmt.Println("Finish")
 }
