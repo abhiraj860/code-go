a447f8366a72f1b2934c9ecd1e8fc7b3d589881a
abhiraj <abhiaditya860@gmail.com>
2025-12-24 20:14:24 +0530

    sleeps

--- main.go
+++ main.go
@@ -1,40 +1,18 @@
-package main 
+package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
-type Node struct {
-	val int
-	next * Node
-}
-
-type LinkedList struct {
-	head *Node
-}
-
-func (list *LinkedList) addNodes(p * Node) {
-	p.next = list.head
-	list.head  = p
-}
-
-func (list * LinkedList) printNodes() {
-	itr := list.head
-	for itr != nil {
-		fmt.Println(itr.val)
-		itr = itr.next
+func say(s string ) {
+	for i := 1; i <= 5; i++ {
+		time.Sleep(500 * time.Millisecond)
+		fmt.Println(s)
 	}
 }
 
 func main() {
-	node1 := Node{val:1}
-	node2 := Node{val:2}
-	node3 := Node{val:3}
-	node4 := Node{val:4}
-
-	list := LinkedList{}
-	list.addNodes(&node1)
-	list.addNodes(&node2)
-	list.addNodes(&node3)
-	list.addNodes(&node4)
-
-	list.printNodes()
+	say("Hell0")
+	say("golang")
 }
