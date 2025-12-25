0752a6cf6855549e7c19aaafef5feeef129b548d
abhiraj <abhiaditya860@gmail.com>
2025-12-25 16:24:51 +0530

    async calls

--- main.go
+++ main.go
@@ -1,28 +1,47 @@
 package main
 
 import (
-	"sync"
 	"fmt"
+	"sync"
+	"time"
 )
 
 func main() {
-	signalChannel := make(chan bool)
-	wg := sync.WaitGroup{}
-	
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		fmt.Println("Goroutine 1 is waiting for signal...")
-		<-signalChannel
-		fmt.Println("Gorouting 1 received the signal and is now doing something")
-	} ()
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		fmt.Println("Goroutine 2 is about to send a signal...")
-		signalChannel <- true
-		fmt.Println("Gourouting 2 send the signal.")
-	} ()
+	now := time.Now()
+	userId := 10
+	respch := make(chan string, 3)
+
+	wg := &sync.WaitGroup{}
+
+	wg.Add(3)
+	go fetchUserData(userId, respch, wg)
+	go fetchUserRecommendation(userId, respch, wg)
+	go fetchUserLike(userId, respch, wg)
+
 	wg.Wait()
-	fmt.Println("Both goroutines have finished")
+	close(respch)
+
+	for resp := range respch {
+		fmt.Println(resp)
+	}
+
+	fmt.Println(time.Since(now))
+}
+
+func fetchUserData(userID int, respch chan string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	time.Sleep(80 * time.Millisecond)
+	respch <- "user data"
+}
+
+func fetchUserRecommendation(userID int, respch chan string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	time.Sleep(120 * time.Millisecond)
+	respch <- "user recommendation"
+}
+
+func fetchUserLike(userID int, respch chan string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	time.Sleep(50 * time.Millisecond)
+	respch <- "user likes"
 }
