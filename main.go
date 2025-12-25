72a408c1edf86ea106f3eb65da45bf50eb401ba5
abhiraj <abhiaditya860@gmail.com>
2025-12-26 01:22:38 +0530

    select

--- main.go
+++ main.go
@@ -4,48 +4,28 @@ import (
 	"fmt"
 )
 
-func main() {
-	fmt.Println("Abe")
+func someFunc(num string) {
+	fmt.Println(num)
 }
 
+func main() {
+	mych := make(chan string)
+	anotherChannel := make(chan string)
 
+	go func() {
+		mych <- "data"
+	} ()
 
-// func main() {
-// 	now := time.Now()
-// 	userId := 10
-// 	respch := make(chan string, 3)
+	go func() {
+		anotherChannel <- "cow"
+	} ()	
 
-// 	wg := &sync.WaitGroup{}
+	select {
+	case msg:= <-mych:
+		fmt.Println(msg)
+	case anotherChannelmsg := <-anotherChannel:
+		fmt.Println(anotherChannelmsg)
+	}
 
-// 	wg.Add(3)
-// 	go fetchUserData(userId, respch, wg)
-// 	go fetchUserRecommendation(userId, respch, wg)
-// 	go fetchUserLike(userId, respch, wg)
+}
 
-// 	wg.Wait()
-// 	close(respch)
-
-// 	for resp := range respch {
-// 		fmt.Println(resp)
-// 	}
-
-// 	fmt.Println(time.Since(now))
-// }
-
-// func fetchUserData(userID int, respch chan string, wg *sync.WaitGroup) {
-// 	defer wg.Done()
-// 	time.Sleep(80 * time.Millisecond)
-// 	respch <- "user data"
-// }
-
-// func fetchUserRecommendation(userID int, respch chan string, wg *sync.WaitGroup) {
-// 	defer wg.Done()
-// 	time.Sleep(120 * time.Millisecond)
-// 	respch <- "user recommendation"
-// }
-
-// func fetchUserLike(userID int, respch chan string, wg *sync.WaitGroup) {
-// 	defer wg.Done()
-// 	time.Sleep(50 * time.Millisecond)
-// 	respch <- "user likes"
-// }
