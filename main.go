61d9b7ce3f45c5213eeaa141e622d0a1a337b653
abhiraj <abhiaditya860@gmail.com>
2025-12-25 10:06:38 +0530

    atomic

--- main.go
+++ main.go
@@ -1,39 +1,28 @@
 package main
 
 import (
-	"log"
-	"runtime"
+	"fmt"
 	"sync"
+	"sync/atomic"
 )
 
 func main() {
-	times := 0
-	for {
-		times++
-		value := PackItems(0)
-		if value != 2000 {
-			log.Fatalf("if should be 2000 but but found %d on execution of counter %d", value, times)
-			break
-		}
-	}
+	value := PackItems(0)
+	fmt.Println(value)
 }
 
-func PackItems(totalItem int) int {
+func PackItems(totalItem int32) int32 {
 	const workers = 2
 	const itemsPerWorker = 1000
 
 	wg := sync.WaitGroup{}
-	itemsPacked := 0
 
 	for i := 0; i < workers; i++ {
 		wg.Add(1)
 		go func(workerID int) {
 			defer wg.Done()
 			for j := 0; j < itemsPerWorker; j++ {
-				itemsPacked = totalItem
-				itemsPacked++
-				runtime.Gosched()
-				totalItem = itemsPacked
+				atomic.AddInt32(&totalItem, 1)
 			}
 		}(i)
 	}
--- main_test.go
+++ main_test.go
@@ -2,11 +2,10 @@ package main
 
 import "testing"
 
-func TestPackItems(t * testing.T) {
+func TestPackItems(t *testing.T) {
 	totalItems := PackItems(2000)
 	expectedTotal := 2000
-	if totalItems != expectedTotal {
+	if totalItems != int32(expectedTotal) {
 		t.Errorf("Expected total: %d, Actual value %d", expectedTotal, totalItems)
 	}
 }
-
