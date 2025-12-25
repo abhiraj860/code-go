cbc5eb4b6835ec1253866e13f266fb2465cdf29d
abhiraj <abhiaditya860@gmail.com>
2025-12-25 11:05:04 +0530

    still a deadlock situation as the sender channel and receiver channels are not sready simultaneously

--- main.go
+++ main.go
@@ -1,36 +1,9 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-)
+import "fmt"
 
 func main() {
-	value := PackItems(0)
-	fmt.Println(value)
-}
-
-func PackItems(totalItem int) int {
-	const workers = 2
-	const itemsPerWorker = 1000
-
-	mu := sync.Mutex{}
-	wg := sync.WaitGroup{}
-	itemsPacked := 0
-	for i := 0; i < workers; i++ {
-		wg.Add(1)
-		go func(workerID int) {
-			defer wg.Done()
-			for j := 0; j < itemsPerWorker; j++ {
-				mu.Lock()
-				itemsPacked = totalItem
-				itemsPacked++
-				totalItem = itemsPacked
-				mu.Unlock()
-			}
-		}(i)
-	}
-
-	wg.Wait()
-	return totalItem
+	c := make(chan string)
+	c <- "message" // Sending
+	fmt.Println(<-c) // Receiving
 }
