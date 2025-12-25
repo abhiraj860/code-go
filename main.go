76b5ebb393b5de468a6f7646601dacd4d8ba493f
abhiraj <abhiaditya860@gmail.com>
2025-12-25 11:31:40 +0530

    two go

--- main.go
+++ main.go
@@ -3,7 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	c := make(chan string)
-	c <- "message" // Sending
-	fmt.Println(<-c) // Receiving
+	balls := make(chan string)
+	go throwBall("red", balls)
+	go throwBall("green", balls)
+	fmt.Println(<-balls, "received")
+	fmt.Println(<-balls, "received")
+}
+
+func throwBall(color string, balls chan string) {
+	fmt.Printf("Throwing string, ball %s ball \n", color)
+	balls <- color
 }
