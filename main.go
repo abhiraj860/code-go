d7f3fd0cc5e94696a45ea08ffdcc18b9a26e0391
abhiraj <abhiaditya860@gmail.com>
2025-12-26 19:52:08 +0530

    complete clid

--- main.go
+++ main.go
@@ -2,15 +2,52 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 )
 
-func app(words []string) {
-	for _,w := range words {
-		if len(w) % 2 == 0 {
-			fmt.Fprintf(os.Stdout, "word %s is even \n", w)
+type Option func(*CliConfig) error
+
+func WithErrStream(errStream io.Writer) Option {
+	return func(c *CliConfig) error {
+		c.ErrStream = errStream
+		return nil
+	}
+}
+
+func WithOutStream(outStream io.Writer) Option {
+	return func(c *CliConfig) error {
+		c.OutStream = outStream
+		return nil
+	}
+}
+
+type CliConfig struct {
+	ErrStream io.Writer
+	OutStream io.Writer
+}
+
+func NewCliConfig(opt ...Option) (CliConfig, error) {
+	c := CliConfig{
+		ErrStream: os.Stderr,
+		OutStream: os.Stdout,
+	}
+
+	for _, opt := range opt {
+		if err := opt(&c); err != nil {
+			return CliConfig{}, err
+		}
+	}
+
+	return c, nil
+}
+
+func app(words []string, cfg CliConfig) {
+	for _, w := range words {
+		if len(w)%2 == 0 {
+			fmt.Fprintf(cfg.OutStream, "word %s is even \n", w)
 		} else {
-			fmt.Fprintf(os.Stderr, "word %s is odd \n", w)
+			fmt.Fprintf(cfg.ErrStream, "word %s is odd \n", w)
 		}
 	}
 }
@@ -21,5 +58,10 @@ func main() {
 		fmt.Fprintln(os.Stderr, "No words provided.")
 		os.Exit(1)
 	}
-	app(words)
+	cfg, err := NewCliConfig()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error creating config: %v\n", err)
+		os.Exit(1)
+	}
+	app(words, cfg)
 }
