17ae32ed048cc0bdff2ff8cec860780b7382ed6a
abhiraj <abhiaditya860@gmail.com>
2025-12-27 11:05:56 +0530

    file state

--- main.go
+++ main.go
@@ -1,72 +1,21 @@
-// Declare the main package
-package main
+package main 
 
-// Import the fmt, io, and os package
 import (
 	"fmt"
-	"io"
 	"os"
 )
 
-// Declares the CliConfig struct having the ErrStream and Outstream
-type CliConfig struct {
-	ErrStream io.Writer
-	OutStream io.Writer
-}
-
-type Option func(*CliConfig) error
-
-func NewCliConfig(opt ...Option) (CliConfig, error) {
-	c := CliConfig{
-		ErrStream: os.Stderr,
-		OutStream: os.Stdout,
-	}
-
-	for _, opt := range opt {
-		if err := opt(&c); err != nil {
-			return CliConfig{}, err
-		}
-	}
-
-	return c, nil
-}
-
-func WithErrStream(errStream io.Writer) Option {
-	return func(c *CliConfig) error {
-		c.ErrStream = errStream
-		return nil
-	}
-}
-
-func WithOutStream(outStream io.Writer) Option {
-	return func(c *CliConfig) error {
-		c.OutStream = outStream
-		return nil
-	}
-}
-
-
-
-func app(words []string, cfg CliConfig) {
-	for _, w := range words {
-		if len(w)%2 == 0 {
-			fmt.Fprintf(cfg.OutStream, "word %s is even \n", w)
-		} else {
-			fmt.Fprintf(cfg.ErrStream, "word %s is odd \n", w)
-		}
-	}
-}
-
 func main() {
-	words := os.Args[1:]
-	if len(words) == 0 {
-		fmt.Fprintln(os.Stderr, "No words provided.")
-		os.Exit(1)
-	}
-	cfg, err := NewCliConfig()
+	info, err := os.Stat("../LLD/airlinemanagementsystem/aircraft.go")
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error creating config: %v\n", err)
-		os.Exit(1)
-	}
-	app(words, cfg)
+		if os.IsNotExist(err) {
+			fmt.Println("File does not exist")
+		} else {
+			panic(err)
+		}
+	} 
+	fmt.Printf("File name: %s \n",info.Name())
+	fmt.Printf("File Size: %d \n", info.Size())
+	fmt.Printf("File permission: %s \n", info.Mode())
+	fmt.Printf("Last Modified: %s \n", info.ModTime())	
 }
