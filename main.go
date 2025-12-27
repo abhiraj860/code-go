1db19a3fa43cf7a14beacece045d2c633981031b
abhiraj <abhiaditya860@gmail.com>
2025-12-27 14:41:25 +0530

    octal permission

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-package main 
+package main
 
 import (
 	"fmt"
@@ -6,19 +6,18 @@ import (
 )
 
 func main() {
-	info, err := os.Stat("../LLD/airlinemanagementsystem")
+	fileInfo, err := os.Stat("../Buffers/huge-allocation.s")
+
 	if err != nil {
 		if os.IsNotExist(err) {
 			fmt.Println("File does not exist")
-		} else {
-			panic(err)
+			return
 		}
-	} 
-	fmt.Printf("File name: %s \n",info.Name())
-	fmt.Printf("File Size: %d \n", info.Size())
-	fmt.Printf("File permission: %o \n", info.Mode().Perm())
-	fmt.Printf("Last Modified: %s \n", info.ModTime())
-	fmt.Printf("File is regular %v \n", info.Mode().IsRegular())	
-	fmt.Printf("File is directory %v \n", info.IsDir())	
-	fmt.Printf("File is directory %v \n", os.ModeSymlink)	
+		fmt.Println("Error: ", err)
+		return
+	}
+	
+	permissions := fileInfo.Mode().Perm()
+	permissionString := fmt.Sprintf("%o", permissions)
+	fmt.Printf("Permission %s\n", permissionString)
 }
