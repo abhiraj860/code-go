c62d2452381cf5e523a1775b82fca69d9e5f022a
abhiraj <abhiaditya860@gmail.com>
2025-12-27 13:29:35 +0530

    done

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	info, err := os.Stat("../LLD/airlinemanagementsystem/aircraft.go")
+	info, err := os.Stat("../LLD/airlinemanagementsystem")
 	if err != nil {
 		if os.IsNotExist(err) {
 			fmt.Println("File does not exist")
@@ -16,6 +16,9 @@ func main() {
 	} 
 	fmt.Printf("File name: %s \n",info.Name())
 	fmt.Printf("File Size: %d \n", info.Size())
-	fmt.Printf("File permission: %s \n", info.Mode())
-	fmt.Printf("Last Modified: %s \n", info.ModTime())	
+	fmt.Printf("File permission: %o \n", info.Mode().Perm())
+	fmt.Printf("Last Modified: %s \n", info.ModTime())
+	fmt.Printf("File is regular %v \n", info.Mode().IsRegular())	
+	fmt.Printf("File is directory %v \n", info.IsDir())	
+	fmt.Printf("File is directory %v \n", os.ModeSymlink)	
 }
