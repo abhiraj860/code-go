fa2fb9fd380a31db3a3bbab5f8316df7f955a5f5
abhiraj <abhiaditya860@gmail.com>
2025-12-28 11:46:15 +0530

    args filter

--- main.go
+++ main.go
@@ -2,37 +2,66 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"os"
 )
 
-func sliceToChannel(inp []int) chan int {
-	ch := make(chan int)
-	go func() {
-		for _, v := range inp {
-			ch <- v
-		}
-		close(ch)
-	}()
-	return ch
+type Option func(*CliConfig) error
+
+type CliConfig struct {
+	ErrStream io.Writer
+	OutStream io.Writer
 }
 
-func sq(data chan int) chan int {
-	p := make(chan int)
-	go func() {
-		for itr := range data {
-			p <- (itr * itr)
+func NewCliConfig(opts ...Option) (CliConfig, error) {
+	c := CliConfig{
+		ErrStream: os.Stderr,	
+		OutStream: os.Stdout,
+	}
+
+	for _, op := range opts {
+		if err := op(&c); err != nil {
+			return CliConfig{}, err
 		}
-		close(p)
-	} ()
-	return p
+	}
+
+	return c, nil
+}
+
+func UpdateErrStream(inpErrStream io.Writer) Option {
+	return func(c * CliConfig) error {
+		c.ErrStream = inpErrStream
+		return nil
+	}
+}
+
+func UpdateOutStream(outStream io.Writer) Option {
+	return func(c * CliConfig) error {
+		c.OutStream = outStream
+		return nil
+	}
+}
+
+func app(argsValues []string, cliconfig CliConfig) {
+	for _, val := range argsValues {
+		if len(val)%2 == 0 {
+			fmt.Fprintf(cliconfig.OutStream, "Word %s is even\n", val)
+			return
+		} else {
+			fmt.Fprintf(cliconfig.ErrStream, "%Word %s is odd\n", val)
+		}
+	}
 }
 
 func main() {
-	inp := []int{2, 3, 4, 7, 1}
-
-	dataChannel := sliceToChannel(inp)
-	finalChannel := sq(dataChannel)
-
-	for itr := range finalChannel {
-		fmt.Println(itr)
+	words := os.Args[1:]
+	if len(words) == 0 {
+		fmt.Fprintf(os.Stderr, "No words provided")
+		os.Exit(1) 
+	}	
+	cfg, err := NewCliConfig()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error creating config: %v\n", err)
 	}
+	app(words, cfg)
 }
