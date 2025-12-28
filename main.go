5864aa5cb935316d065b4b086341ebd3259b0b23
abhiraj <abhiaditya860@gmail.com>
2025-12-28 22:59:55 +0530

    modifie

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io"
 	"os"
@@ -9,12 +10,14 @@ import (
 type Option func(*CliConfig) error
 
 type CliConfig struct {
+	OutputFile string
 	ErrStream io.Writer
 	OutStream io.Writer
 }
 
 func NewCliConfig(opts ...Option) (CliConfig, error) {
 	c := CliConfig{
+		OutputFile: "",
 		ErrStream: os.Stderr,	
 		OutStream: os.Stdout,
 	}
@@ -42,26 +45,44 @@ func UpdateOutStream(outStream io.Writer) Option {
 	}
 }
 
-func app(argsValues []string, cliconfig CliConfig) {
+func app(argsValues []string, outputWriter io.Writer) {
 	for _, val := range argsValues {
 		if len(val)%2 == 0 {
-			fmt.Fprintf(cliconfig.OutStream, "Word %s is even\n", val)
+			fmt.Fprintf(outputWriter, "Word %s is even\n", val)
 			return
 		} else {
-			fmt.Fprintf(cliconfig.ErrStream, "%Word %s is odd\n", val)
+			fmt.Fprintf(outputWriter, "Word %s is odd\n", val)
 		}
 	}
 }
 
 func main() {
+	var outputFileName string 
+	var outputWriter io.Writer
+	
+	flag.StringVar(&outputFileName, "f", "", "Output file (default:stdout)")
+	flag.Parse()
+	
+	fmt.Println(outputFileName)	
 	words := os.Args[1:]
 	if len(words) == 0 {
 		fmt.Fprintf(os.Stderr, "No words provided")
 		os.Exit(1) 
-	}	
-	cfg, err := NewCliConfig()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error creating config: %v\n", err)
+		}	
+		cfg, err := NewCliConfig()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error creating config: %v\n", err)
+		}
+		if cfg.OutputFile != "" {
+			outputFile, err := os.Create(cfg.OutputFile)
+		if err != nil {
+			fmt.Fprintf(cfg.ErrStream, "Error Creating output file: %v\n", err)
+			os.Exit(1)
+		}
+		defer outputFile.Close()
+		outputWriter = io.MultiWriter(cfg.OutStream, outputFile)
+	} else {
+		outputWriter = cfg.OutStream
 	}
-	app(words, cfg)
+	app(words, outputWriter)
 }
