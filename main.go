9da188efc1029da4e4b68774758c4af664a79664
abhiraj <abhiaditya860@gmail.com>
2025-12-29 22:06:19 +0530

    walk dir

--- main.go
+++ main.go
@@ -5,21 +5,22 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"path/filepath"
 )
 
 type Option func(*CliConfig) error
 
 type CliConfig struct {
 	OutputFile string
-	ErrStream io.Writer
-	OutStream io.Writer
+	ErrStream  io.Writer
+	OutStream  io.Writer
 }
 
 func NewCliConfig(opts ...Option) (CliConfig, error) {
 	c := CliConfig{
 		OutputFile: "",
-		ErrStream: os.Stderr,	
-		OutStream: os.Stdout,
+		ErrStream:  os.Stderr,
+		OutStream:  os.Stdout,
 	}
 
 	for _, op := range opts {
@@ -32,54 +33,62 @@ func NewCliConfig(opts ...Option) (CliConfig, error) {
 }
 
 func UpdateErrStream(inpErrStream io.Writer) Option {
-	return func(c * CliConfig) error {
+	return func(c *CliConfig) error {
 		c.ErrStream = inpErrStream
 		return nil
 	}
 }
 
 func UpdateOutStream(outStream io.Writer) Option {
-	return func(c * CliConfig) error {
+	return func(c *CliConfig) error {
 		c.OutStream = outStream
 		return nil
 	}
 }
 
-func app(argsValues []string, outputWriter io.Writer) {
-	for _, val := range argsValues {
-		if len(val)%2 == 0 {
-			fmt.Fprintf(outputWriter, "Word %s is even\n", val)
-			return
-		} else {
-			fmt.Fprintf(outputWriter, "Word %s is odd\n", val)
+func app(directories []string, outputWriter io.Writer, cfg *CliConfig) {
+	for _, directory := range directories {
+		err := filepath.WalkDir(directory, func(path string, d os.DirEntry, err error) error {
+			if path == ".git" {
+				return filepath.SkipDir
+			}
+			if d.IsDir() {
+				fmt.Fprintf(outputWriter, "%s\n", path)
+			}
+			return nil
+		})
+		if err != nil {
+			fmt.Fprintf(cfg.ErrStream, "Error walking the path %q: %v\n", directory, err)
+			continue
 		}
 	}
 }
 
 func main() {
-	var outputFileName string 
+	var outputFile string
 	var outputWriter io.Writer
-	
-	flag.StringVar(&outputFileName, "f", "", "Output file (default:stdout)")
+
+	flag.StringVar(&outputFile, "f", "", "Output file (default:stdout)")
 	flag.Parse()
-	
+
 	directories := os.Args[1:]
-	
-	if len(outputFileName) != 0 {
+
+	if len(outputFile) != 0 {
 		directories = os.Args[3:]
 	}
-	fmt.Println(directories)	
-	
+
 	if len(directories) == 0 {
 		fmt.Fprintf(os.Stderr, "No words provided")
-		os.Exit(1) 
-		}	
-		cfg, err := NewCliConfig()
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error creating config: %v\n", err)
-		}
-		if cfg.OutputFile != "" {
-			outputFile, err := os.Create(cfg.OutputFile)
+		os.Exit(1)
+	}
+	cfg, err := NewCliConfig()
+	cfg.OutputFile = outputFile
+	
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error creating config: %v\n", err)
+	}
+	if cfg.OutputFile != "" {
+		outputFile, err := os.Create(cfg.OutputFile)
 		if err != nil {
 			fmt.Fprintf(cfg.ErrStream, "Error Creating output file: %v\n", err)
 			os.Exit(1)
@@ -89,5 +98,5 @@ func main() {
 	} else {
 		outputWriter = cfg.OutStream
 	}
-	app(directories, outputWriter)
+	app(directories, outputWriter, &cfg)
 }
